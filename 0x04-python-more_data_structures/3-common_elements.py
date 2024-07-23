ee281345f84aa4b36a5e61b1a5f76aaf8961d7e0
254CMN <collinsnzei6@gmail.com>
2024-07-23 13:28:05 +0300

    Changes made to file 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    """Returns a set of common elements from two sets"""
+    """Returns a set of common elements from two sets
 
+    """
     set_3 = {x  for x in set_1 for y in set_2 if x == y and  x in set_2}
-
     return set_3
