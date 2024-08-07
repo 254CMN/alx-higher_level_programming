6855d2c4b589c563f31785aafdd541b63d8974da
254CMN <collinsnzei6@gmail.com>
2024-08-07 08:42:22 -0700

    Solutions to tasks 3 to 6 for SQL intro.

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table first_table with attributes id and name
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT PRIMARY KEY AUTO INCREMENT,
+	name VARCHAR(256)
+);
+
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Lists all rows from the table first_table in the database hbtn_0c_0 in my MySQL server
+SELECT *
+FROM first_table;
+
