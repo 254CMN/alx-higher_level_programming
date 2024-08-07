61e4a0bc29b7dfcc325335291cf537f8ea6e67e8
254CMN <collinsnzei6@gmail.com>
2024-08-07 09:24:09 -0700

    Solutions to tasks 5 to 10.

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- Creates a table first_table with attributes id and name
-CREATE TABLE 'first_table'(
-	'id' INT PRIMARY KEY AUTO INCREMENT,
+CREATE TABLE first_table(
+	'id' INT DEFAULT NULL,
 	'name' VARCHAR(256) DEFAULT NULL
 );
 
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in my MySQL server without using DESCRIBE or EXPLAIN.
-SHOW CREATE TABLE 'hbtn_0c_0.first_table';
+SHOW CREATE TABLE hbtn_0c_0.first_table;
 
