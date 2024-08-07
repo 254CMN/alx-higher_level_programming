a49c2e9111ac5e81e57572db9d805bcb2fcb778f
254CMN <collinsnzei6@gmail.com>
2024-08-07 08:55:31 -0700

    Corrections to tasks 4 and 5.

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- Creates a table first_table with attributes id and name
-CREATE TABLE first_table(
-	id INT PRIMARY KEY AUTO INCREMENT,
-	name VARCHAR(256)
+CREATE TABLE 'first_table'(
+	'id' INT PRIMARY KEY AUTO INCREMENT,
+	'name' VARCHAR(256) DEFAULT NULL
 );
 
