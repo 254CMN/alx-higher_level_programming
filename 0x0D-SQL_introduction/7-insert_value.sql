e87fd9a4d450d861bec14e2f0d4fbeefee61a4e6
254CMN <collinsnzei6@gmail.com>
2024-08-09 02:59:50 -0700

    Corrections to task 7 and 8.

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
 --  inserts a new row in the table first_table (database hbtn_0c_0) in my MySQL server
-INSERT INTO hbtn_0c_0.first_table (id, name)
-VALUES (89, 'Best School');
+INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (89, 'Best School');
 
