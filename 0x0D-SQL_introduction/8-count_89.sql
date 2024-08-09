8cfd26692ec976088e86432b2e6a72d53f0ffea8
254CMN <collinsnzei6@gmail.com>
2024-08-09 03:14:49 -0700

    Solution to task 9.

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,6 @@
--- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
-SELECT COUNT (*) FROM hbtn_0c_0.first_table WHERE id = 89;
+-- displays the number of records with id = 89 in the table first_table
+-- of the database hbtn_0c_0 in your MySQL server.
+SELECT COUNT (*)
+FROM hbtn_0c_0.first_table
+WHERE id = 89;
 
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
-CREATE TABLE hbtn_0c_0.second_table(
-	'id' INT DEFAULT NULL,
-	'name' VARCHAR(256) DEFAULT NULL,
-	'score' INT DEFAULT NULL
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table(
+	id INT DEFAULT NULL,
+	name VARCHAR(256) DEFAULT NULL,
+	score INT DEFAULT NULL
 );
 INSERT INTO second_table (id, name, score)
 VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
